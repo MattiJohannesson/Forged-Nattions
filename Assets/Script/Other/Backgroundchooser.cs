d933700d9498d12c28319f1e6ab47bc439edc4e5
MattiJohannesson <tripple_eg@hotmail.co.uk>
2017-07-29 15:38:27 +0100

    additions to the main page
    added the backgrounds to the main screen
    added some code and started the main screen functionality

---
+++ Assets/Script/Other/Backgroundchooser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Backgroundchooser : MonoBehaviour {
+    public int Rand;
+    public Sprite[] BackgroundImg;
+    public Object Background;
+
+	// Use this for initialization
+	void Start () {
+        Rand = Random.RandomRange(1, 8);
+
+        this.GetComponent<Image>().sprite = BackgroundImg[Rand];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Other/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class MenuController : MonoBehaviour {
+
+    public Object SPlayer,MPlayer,CBattles,Opt;
+    public Object ImgSplayer, ImgMPlayer, ImgCBattles, ImgOpt;
+
+	public void SplayerPress()
+    {
+        ImgSplayer;
+    }
+
+    public void MplayerPress()
+    {
+
+    }
+
+    public void CBattlesPress()
+    {
+
+    }
+
+    public void OptionsPress()
+    {
+
+    }
+}
